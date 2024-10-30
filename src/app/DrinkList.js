169b3b9b43f9489bda335b2a66f9fc1674e5caa9
juan31antonio <juanantoniocorder32@gmail.com>
2024-10-30 11:14:38 +0100

    Ejercicios hechos

--- src/app/DrinkList.js
+++ src/app/DrinkList.js
@@ -1,18 +1,40 @@
+const te = {
+  partePlanta: 'hoja',
+  contenidoCafeina: '15–70 mg/taza',
+  aniosAntiguedad: '4,000+ años'
+}
+
+const cafe = {
+  partePlanta: 'grano',
+  contenidoCafeina: '80–185 mg/taza',
+  aniosAntiguedad: '1,000+ años'
+}
+
+
 function Drink({ name }) {
+
+  if(name== 'té'){
+    var planta = te
+  }
+  else{
+    var planta = cafe
+  }
     return (
       <section>
         <h1>{name}</h1>
         <dl>
           <dt>Parte de la planta</dt>
-          <dd>{name === 'té' ? 'hoja' : 'grano'}</dd>
+          <dd>{planta.partePlanta}</dd>
           <dt>Contenido de cafeína</dt>
-          <dd>{name === 'té' ? '15–70 mg/taza' : '80–185 mg/taza'}</dd>
+          <dd>{planta.contenidoCafeina}</dd>
           <dt>Antigüedad</dt>
-          <dd>{name === 'té' ? '4,000+ años' : '1,000+ años'}</dd>
+          <dd>{planta.aniosAntiguedad}</dd>
         </dl>
       </section>
     );
   }
+
+
   
   export default function DrinkList() {
     return (
--- src/app/PackaginList2.js
+++ src/app/PackaginList2.js
@@ -1,7 +1,8 @@
 function Item({ name, importance }) {
     return (
       <li className="item">
-        {name}
+        {importance!=0 && <>{name} <em>Importancia: {importance}</em></>}  
+        {importance==0 && name}      
       </li>
     );
   }
